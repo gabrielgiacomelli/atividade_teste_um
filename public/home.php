5a9aa908464cf9326b52941a2d2d2dcc163d79fe
gabrieldacostagiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-06-01 18:50:06 -0300

    add - comentários da página home

--- public/home.php
+++ public/home.php
@@ -1,22 +1,36 @@
 <?php
+
+//inicia a sessão
 session_start();
+
+//verifica se existe uma sessão chamada usuáreio, caso não exista, redireciona para a página de login
 if(!isset($_SESSION["usuario"])){
     header("Location: ../index.php");
+
+    //encerra o script para evitar que o restante do código seja executado
     exit();
 }
 
+//inclui o arquivo de conexão com o banco de dados
 include("../infra/db/connect.php");
 
+//Verifica se o formulário foi enviado
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+//captura os dados do formulário
     $novoUsuario = $_POST['usuario'];
     $novaSenha = $_POST['senha'];
 
+    //Monta o contato SQL para inserir o novo usuário no banco de dados
     $sql = "INSERT INTO usuarios (usuario,senha) 
     VALUES ('$novoUsuario','$novaSenha')";  
 
+//Executa a consulta SQL e verifica se foi bem sucedida
     if($conn->query($sql) === TRUE){
         echo "<script> alert('Usuário cadastrado com sucesso!')</script>";
     }else{
+
+    //Se ocorrer um erro ao executar a consulta SQL, exibe uma mensagem de erro
         echo "<script> alert('Erro ao cadastrar')</script>";
     }
 
@@ -45,6 +59,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <br>
         <?php
         
+        //Verifica se existe uma variável de erro e, se existir, exibe a mensagem de erro
             if(isset($erro)){
                 echo $erro;
             };
@@ -55,7 +70,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </form>
     <hr>
     <?php
-    
+    //Inclui o arquivo da tabela de usuários, que exibe a lista de usuários cadastrados no banco de dados
     include("components/table.php")
 
     ?>

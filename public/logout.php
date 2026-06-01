1ed4fb39b4cb61486a605713fae868f903c91b01
gabrieldacostagiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-06-01 18:51:22 -0300

    add - comentários página logout

--- public/logout.php
+++ public/logout.php
@@ -1,8 +1,18 @@
 <?php
 
+    // Inicia ou acessa a sessão atual.
+    // É necessário para que o PHP consiga manipular a sessão.
     session_start();
+
+    // Remove todos os dados da sessão.
+    // Na prática, "desloga" o usuário.
     session_destroy();
+
+    // Redireciona o usuário para a página de login.
     header("Location: ../index.php");
+
+    // Interrompe a execução do código.
+    // Garante que nada mais seja executado após o redirecionamento.
     exit();
 
 ?>
